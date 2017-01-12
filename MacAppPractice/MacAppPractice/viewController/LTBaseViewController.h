d66623425e2ab10637233420bebb8873f24ef7de
liaoting0725 <liaoting0725@hotmail.com>
2017-01-12 20:25:01 +0800

    创建LTBaseViewController基类

---
+++ MacAppPractice/MacAppPractice/viewController/LTBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTBaseViewController.h
+//  MacAppPractice
+//
+//  Created by 廖挺 on 2017/1/12.
+//  Copyright © 2017年 liaoting. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LTBaseViewController : NSViewController
+
+@end
---
+++ MacAppPractice/MacAppPractice/viewController/LTBaseViewController.m
@@ -0,0 +1,22 @@
+//
+//  LTBaseViewController.m
+//  MacAppPractice
+//
+//  Created by 廖挺 on 2017/1/12.
+//  Copyright © 2017年 liaoting. All rights reserved.
+//
+
+#import "LTBaseViewController.h"
+
+@interface LTBaseViewController ()
+
+@end
+
+@implementation LTBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end

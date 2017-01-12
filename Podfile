b55b3dfdfc7da9f386f66b37dbe0cacf3b0fb8a8
liaoting0725 <liaoting0725@hotmail.com>
2017-01-12 20:24:19 +0800

    添加pod并注入AFNetworking三方库

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'MacAppPractice' do
+pod 'AFNetworking', '~> 3.1.0'
+project 'MacAppPractice/MacAppPractice.xcodeproj'
+end
---
+++ Pods/Target Support Files/Pods-MacAppPractice/Pods-MacAppPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MacAppPractice : NSObject
+@end
+@implementation PodsDummy_Pods_MacAppPractice
+@end
